969cac8685dfb7e5ae70bfc5d1d3573ce020607d
jason861014 <jason30615@gmail.com>
2023-12-14 18:43:33 +0800

    商品功能-實作新增商品

--- springboot-mall/src/main/java/com/jasonlin/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/jasonlin/springbootmall/dao/ProductDao.java
@@ -1,8 +1,12 @@
 package com.jasonlin.springbootmall.dao;
 
+import com.jasonlin.springbootmall.dto.ProductRequest;
 import com.jasonlin.springbootmall.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/jasonlin/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/jasonlin/springbootmall/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.jasonlin.springbootmall.service;
 
+import com.jasonlin.springbootmall.dto.ProductRequest;
 import com.jasonlin.springbootmall.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
